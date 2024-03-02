56862a4039ae74f837a979793098e5b3e61e9e4d
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-02 21:21:55 +0530

    initial commit

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,6 @@
+import SigninComponent from "@/component/SigninComponent";
+
+export default function Signin() {
+    return <div><SigninComponent /></div>
+}
+
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Signup() {
+  return (
+    <div className="h-screen w-screen bg-black text-white">this is signup page</div>
+  );
+}
